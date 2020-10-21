667ffe8ee4328eee41a3861d3a72642cde0dcc34
Sergio Moreno <sergiomoreno433@gmail.com>
2020-10-21 18:35:45 -0500

    funcion burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,16 @@
 package main
 
 func Burbuja(s []int64)  {
-
+	for i := 0; i <len(s); i++ {
+		for j:= 0; j < len(s)-1; j++{
+			e := s[j]
+			if e > s[j+1]{
+				s[j] = s[j+1]
+				s[j+1] = e
+			}
+		}
+	}
 }
 
 func main()  {
-	
 }
